4387fb9f0c59f6ead8f238e71bd6717d2336f80e
Aatrick <113598245+Aatrick@users.noreply.github.com>
2024-09-12 21:58:35 +0200

    fixed status changer

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import sched, time
+import threading
 import discord
 from discord.ext import tasks
 import os
@@ -5,43 +7,55 @@ import dotenv
 import random
 import glob
 import sys
+
 try:
-    sys.path.insert(1, 'C:\\Dev\\LightDiffusion\\')
+    sys.path.insert(1, "C:\\Dev\\LightDiffusion\\")
     from LightDiffusion import pipeline
-except: print("LightDiffusion not available or incorrectly accessed, proceeding without generative AI")
+except:
+    print(
+        "LightDiffusion not available or incorrectly accessed, proceeding without generative AI"
+    )
 
 dotenv.load_dotenv()
-TOKEN=os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 bot = discord.Bot()
 
-states = ['Sleeping', 'Eating', 'Esport', 'Deadge']
+states = ["Sleeping", "Eating", "Esport", "Deadge"]
 probabilities = [0.8, 0.0999995, 0.0999995, 0.000001]
-current_state = 'Waking up'
+current_state = "Waking up"
+
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
-    
+    print("We have logged in as {0.user}".format(bot))
+    status_task.start()
+
 @tasks.loop(minutes=1)
-async def change_status():
-    global current_state
-    if current_state == 'Dying':
-        change_status.cancel()
+async def status_task():
+    global current_state, states
+    if current_state == "Deadge":
+        status_task.cancel()
         return
     current_state = random.choices(states, weights=probabilities, k=1)[0]
-    await bot.change_presence(activity=discord.Game(name=current_state))
+    await bot.change_presence(
+        status=discord.Status.online, activity=discord.Game(current_state)
+    )
+
 
 @bot.command()
-async def generate(ctx, prompt: discord.Option(str), width: discord.Option(int), height: discord.Option(int)):
+async def generate(
+    ctx,
+    prompt: discord.Option(str),
+    width: discord.Option(int),
+    height: discord.Option(int),
+):
     await ctx.respond(f"here's a picture of : {prompt}")
     pipeline(prompt, width, height)
-    image_files = glob.glob('C:\\Dev\\LightDiffusion\\_internal\\output\\*.png')
+    image_files = glob.glob("C:\\Dev\\LightDiffusion\\_internal\\output\\*.png")
     print(max(image_files, key=os.path.getctime))
     await ctx.followup.send(file=discord.File(max(image_files, key=os.path.getctime)))
-
 @bot.command()
 async def hello(ctx):
-  await ctx.respond(f"Hello, {ctx.author}!")
-
+    await ctx.respond(f"Hello, {ctx.author}!")
 bot.run(TOKEN)
